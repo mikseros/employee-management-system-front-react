fb4ada423ee4b7d8bac1f641497470abe6106ac5
mikseros <mmadynski@gmail.com>
2022-08-11 12:37:50 +0200

    ListEmployee component created & implemented.

---
+++ src/services/EmployeeService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const EMPLOYE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+
+class EmployeeService {
+
+    getEmployees() {
+        return axios.get(EMPLOYE_API_BASE_URL);
+    }
+}
+
+export default new EmployeeService()
