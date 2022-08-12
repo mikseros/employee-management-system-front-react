dfa61630ed452b3fa27d95271e418a799017173e
mikseros <mmadynski@gmail.com>
2022-08-12 23:25:28 +0200

    UpdateEmployee component created

---
+++ src/components/UpdateEmployeeComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UpdateEmployeeComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>TEST</h1>
+            </div>
+        );
+    }
+}
+
+export default UpdateEmployeeComponent;
