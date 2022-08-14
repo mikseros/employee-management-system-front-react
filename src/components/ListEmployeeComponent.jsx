a4fd2b3c51650bcb1719b1766a7640b338b35b60
mikseros <mmadynski@gmail.com>
2022-08-14 21:15:44 +0200

    Button for ViewEmployeeComponent created

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -64,6 +64,7 @@ class ListEmployeeComponent extends Component {
                     <td>
                       <button onClick={ () => this.editEmployee(employee.id)} className="btn btn-info">Update</button>
                       <button style={{marginLeft: "10px"}} onClick={ () => this.deleteEmployee(employee.id)} className="btn btn-danger">Delete</button>
+                      <button style={{marginLeft: "10px"}} onClick={ () => this.viewEmployee(employee.id)} className="btn btn-success">View</button>
                     </td>
                   </tr>
                 )
